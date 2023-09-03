adc1c50478380aa81edc0359e8a222e854f18d22
Adrian <adrian.hamzah15@gmail.com>
2023-09-03 11:50:31 +0700

    Initial Commit

---
+++ src/app/about/[movieName]/page.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function Users({ params }) {
+  const movieTitle = params.movieName;
+  const jsonFilenames = [movieTitle];
+
+  const movieLinks = jsonFilenames.map((filename) => {
+    const movieData = require(`films/${filename}`);
+    return (
+      <div key={movieData.key} className="flex flex-col justify-center items-center h-screen w-screen gap-2 ">
+        <img src={movieData.posterUrl} alt={movieData.title} className="w-1/2 h-1/3 opacity-80"/>
+        <div className='flex gap-7'>
+          <div className="w-100 h-7">{movieData.title}</div>
+          <button className='group relative w-20 rounded-lg bg-neutral-800 shadow'>
+            <span className='absolute inset-0 w-2 bg-yellow-500  rounded-lg transition-all duration-[500ms] ease-out group-hover:w-full'></span>
+            <div className='relative text-white hover:text-orange-700 font-bold'>Pesan</div>
+          </button>
+          {/* <button className='w-30vw bg-orange-600 border border-x-yellow-500 shadow rounded-lg'>Pesan</button> */}
+          
+        </div>
+        <div>Category: {movieData.category}</div>
+        <div>Age Rating: {movieData.ageRating}</div>
+        <div>Description: {movieData.description}</div>
+      </div>
+    );
+  });
+
+  return <div>{movieLinks}</div>;
+}
+/*
+bikin gap diantaranya
+foto taro tengah terus bikin transparan (opacity)
+title kiri, kanan tombol buat pesen
+bawah title category (tulisan category sama age rating dibold) (dikiri) age rating (dikanan)
+tulisan description nya bold, justify
+*/
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,66 @@
-import Image from 'next/image'
+import React from 'react';
+import fs from 'fs';
+import path from 'path';
+import Link from 'next/link';
+
+function WelcomeText(){
+  return (
+    <div className="mb-2.5">Welcome to Sea Cinema</div>
+  );
+}
+
+function ImageTrack(){
+  //get all the json file
+  const filmsDirectory = path.join(process.cwd(), 'films');
+  const jsonFilenames = fs.readdirSync(filmsDirectory).filter(filename => filename.endsWith('.json'));
+
+  const movieLinks = jsonFilenames.map((filename) => {
+    const movie = require(`films/${filename}`);
+    const movieName = filename.replace(".json","");
+    return (
+      <Link href={`/about/${movieName}`} key={movieName}>
+          <div className="flex-shrink-0 w-[300px] bg-neutral-800 rounded-[8px] border border-sky-900 transition-transform transform hover:scale-105">
+            <div className="relative">
+              <img
+                src={movie.posterUrl}
+                className="w-full object-cover h-[200px] rounded-t-[8px]"
+                alt={movie.title}
+              />
+
+              <div className="text-center text-orange-700">{movie.title}</div>
+              <div className="text-center text-stone-500">Category: {movie.category}</div>
+              <div className='text-center text-yellow-500 border border-yellow-500 rounded-[8px]'>Rating: {movie.ageRating}</div>
+            </div>
+          </div>
+      </Link>
+    );
+  })
+
+  return (
+    <div className="flex gap-3">
+        {movieLinks}
+    </div>
+  );
+
+}
+/*
+SU (All Ages/Semua Umur): Suitable for all viewers from kids to adults, equivalent to TV-G.
+BO (Parental Guidance/Bimbingan Orang Tua): Parental guidance suggested. Equivalent to TV-PG.
+R (Teenager/Remaja): Suitable for viewers aged 13-17, equivalent to TV-14 (low end).
+D (Mature Audience/Dewasa): Suitable for viewers over 17, equivalent to TV-MA.
+*/
+
+function SEACinemaHomePage(){
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <WelcomeText/>
+      <ImageTrack/>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <SEACinemaHomePage/>
   )
 }
